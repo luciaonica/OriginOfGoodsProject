8a5707b5a920ebb8e1c3619ea89f159ffda03d0e
lucia <lucia12onica@gmail.com>
2023-01-24 13:30:41 -0600

    updated sql file

--- src/main/resources/database/origin.sql
+++ src/main/resources/database/origin.sql
@@ -122,7 +122,7 @@ CREATE TABLE `currencies` (
   `name` varchar(64) NOT NULL,
   `symbol` varchar(3) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +131,7 @@ CREATE TABLE `currencies` (
 
 LOCK TABLES `currencies` WRITE;
 /*!40000 ALTER TABLE `currencies` DISABLE KEYS */;
-INSERT INTO `currencies` VALUES (1,'USD','United States Dollar','$');
+INSERT INTO `currencies` VALUES (1,'USD','United States Dollar','$'),(2,'EUR','Euro','€'),(3,'GBP','British Pound','£'),(4,'JPY','Japanese Yen','¥'),(5,'MDL','Moldovan Leu','L');
 /*!40000 ALTER TABLE `currencies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -252,4 +252,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-23 20:15:43
+-- Dump completed on 2023-01-24 12:56:45
